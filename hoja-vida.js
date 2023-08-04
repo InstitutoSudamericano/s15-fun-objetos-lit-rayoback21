c48ff914f3ae1f217ed971bbd3e5936a9f1bbe0d
rayoback21 <129207125+rayoback21@users.noreply.github.com>
2023-08-04 10:41:23 -0500

    tarea semana 15 resuelta

--- hoja-vida.js
+++ hoja-vida.js
@@ -1,2 +1,42 @@
 //Crear variables con diferentes tipos de datos (number, string, array, objetos literales) para almacenar información de una persona, 
 //pasar estos como parámetros de una función, que imprimirá en pantalla usando un template string con backcstick (``).
+let curriculum={
+    
+    address: "Simon Bolivar"
+}
+
+let fullname={
+    nombre: "Bryan Bonilla"
+}
+
+let ci={
+    cedula: "0704448547"
+}
+
+let skills={
+    habilidad: "Eficiente"
+}
+
+let address={
+    Direccion: "AV.Ricaurte y 18 de noviembre"
+}
+
+
+function showCurriculum(fullname,ci,skills,address){
+    let Salida=`
+    Hoja de Vida 
+    fullname: ${fullname.nombre}
+    ci: ${ci.cedula}
+    skills: ${skills.habilidad}
+    address: ${address.Direccion}
+    
+    `
+    
+return Salida;
+}
+let resultado=showCurriculum(fullname,ci,skills,address);
+console.log(resultado);
+
+
+
+
